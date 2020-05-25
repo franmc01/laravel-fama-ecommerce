fb4a73631633b80c2b9406eeb49b7d3f367660b5
ImFrancis98 <francmarinc@gmail.com>
2020-05-25 10:38:39 -0500

    Configurando las migraciones

--- database/migrations/2020_04_30_141847_create_categorias_table.php
+++ database/migrations/2020_04_30_141847_create_categorias_table.php
@@ -16,7 +16,6 @@ class CreateCategoriasTable extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->string('nombre_categoria');
-            $table->timestamps();
         });
     }
 
--- database/migrations/2020_05_25_103116_create_submarcas_table.php
+++ database/migrations/2020_05_25_103116_create_submarcas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMarcasTable extends Migration
+class CreateSubmarcasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateMarcasTable extends Migration
      */
     public function up()
     {
-        Schema::create('marcas', function (Blueprint $table) {
+        Schema::create('submarcas', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre_marca');
-            $table->unsignedBigInteger('categoria_id');
-            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->string('nombre_submarca');
+            $table->unsignedBigInteger('marca_id');
+            $table->foreign('marca_id')->references('id')->on('marcas');
 
         });
     }
@@ -29,6 +29,6 @@ class CreateMarcasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('marcas');
+        Schema::dropIfExists('submarcas');
     }
 }
