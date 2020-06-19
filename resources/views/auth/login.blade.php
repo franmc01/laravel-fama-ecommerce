2988c727a1804a0cf6c3e3cb4d9f2fd9385a2ea9
ImFrancis98 <francmarinc@gmail.com>
2020-06-19 16:33:32 -0500

    Hola si

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,16 +14,14 @@
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" crossorigin="anonymous">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page" style="background-image: url('/img/fondo.jpg');">
 <div class="login-box">
-  <div class="login-logo">
-    <p class="text-muted text-center">ADMINISTRACIÓN</p>
-  </div>
   <!-- /.login-logo -->
   <div class="card" style="height: 350px">
     <div class="card-body login-card-body">
       <p class="login-box-msg">
           <img src="{{ asset('/img/lo1go.png') }}"  height="70px" style="padding-right: 30px">
+          <p class="text-center" style="color:gray; font-weight:bold">Administración</p>
       </p>
 
       <form action="{{ route('login') }}" method="POST">
@@ -56,7 +54,7 @@
         <br>
         <div class="row">
           <div class="col-md-3"></div>
-          <div class="col-6">
+          <div class="col-6 text-center">
             <button type="submit" class="btn btn-primary btn-block">Acceder</button>
           </div>
           <div class="col-md-3"></div>
